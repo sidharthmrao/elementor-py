a25d2ac01f36b64c1ad52546c9e2e5e4ae8e32b4
Sidharth Rao <sidharthmrao@gmail.com>
2022-12-29 03:33:33 -0800

    semiworking combination

--- bruh.py
+++ bruh.py
@@ -72,36 +72,144 @@ except:
 
 
 def analyze_syllable(syllable):
-    pass
+    if syllable in "0123456789":
+        return syllable
+    match len(syllable):
+        case 1:
+            return single_reference[syllable] + "a"
+        case 2:
+            return double_reference[syllable[0]][syllable[1]] + "a"
+        case 3:
+            return double_reference[syllable[0]][syllable[1]] + triple_reference[syllable[2]]
+        case _:
+            return double_reference[syllable[0]][syllable[1]] + triple_reference[syllable[2]] + quad_reference[
+                syllable[3]]
 
-def compile_values(values: list):
-    if not all(value in elements.keys() for value in values):
-        for value in values:
-            if value not in elements.keys():
-                print(f"{value} not found, please initialize this item before using it to make another.")
+
+def analyze_root(root):
+    if type(root) == str:
+        return analyze_syllable(root)
+    elif len(root) == 1:
+        return analyze_syllable(root[0])
     else:
-        values_root = sorted([''.join(elements[values[0]]["root"]), ''.join(elements[values[1]]["root"])])
-        if values_root[0] == values_root[1]:
-            elements[values]["root"] = values_root[0]+"2"
-        else:
-            value_0_split = values_root[0].split(")")[-1][:-1]
-            value_1_split = values_root[1].split(")")[0][1:]
+        a = analyze_root(root[0])
+        b = analyze_root(root[1])
 
-            value_splits = sorted([value_0_split, value_1_split])
+        if a in "0123456789":
+            if b[-1] in "aeiou":
+                return b + "a"
+            elif b[-1] not in "wylr":
+                return b[:-1] + "a" + b[-1]
+            elif b[-1] == "a":
+                return b + "y"
+            elif b[-2]+b[-1] == "ay":
+                return b[:-1] + "w"
+            elif b[-2]+b[-1] == "aw":
+                return b[:-1] + "r"
+            elif b[-2]+b[-1] == "ar":
+                return b[:-1] + "l"
 
-            if len(value_splits[0]+value_splits[1]) <= 4:
-                combine_portion = ''.join(value_splits)
-                elements[values]["root"] = combine_portion
+        return analyze_root(root[0]), analyze_root(root[1])
 
-            else:
-                elements[item]["root"] = ["(" + ''.join(elements[value]["root"]) + ")" for value in values]
+
+def compile_to_string(lis):
+    if type(lis) == str:
+        return lis
+    elif len(lis) == 1:
+        return lis[0]
+
+    if type(lis[0]) == str:
+        string_1 = lis[0]
+    else:
+        string_1 = compile_to_string(lis[0])
+    if type(lis[1]) == str:
+        string_2 = lis[1]
+    else:
+        string_2 = compile_to_string(lis[1])
+
+    return string_1 + string_2
+
+
+def compile_to_visual(lis):
+    if type(lis) == str:
+        return "(" + lis + ")"
+    elif len(lis) == 1:
+        return "(" + lis[0] + ")"
+
+    return "(" + compile_to_visual(lis[0]) + compile_to_visual(lis[1]) + ")"
+
+
+def check_b_greater(a, b):
+    if type(a) == str and type(b) == str:
+        if b.startswith(a):
+            return True
+        elif a.startswith(b):
+            return False
+        return a > b
+    elif type(a) == str:
+        return True
+    elif type(b) == str:
+        return False
+    else:
+        return check_b_greater(a[0], b[0])
+
+
+def custom_sort(item: str):
+    current = elements[item]["root"]
+
+    if type(current) == str:
+        return
+    elif len(current) == 1:
+        elements[item]["root"] = current[0]
+        return
+
+    string_1 = compile_to_string(current[0])
+    string_2 = compile_to_string(current[1])
+
+    if check_b_greater(string_1, string_2):
+        elements[item]["root"] = [current[1], current[0]]
+        elements[item]["base"] = [elements[item]["base"][1], elements[item]["base"][0]]
 
 
 def print_item(item: str):
     print("___")
     print(elements[item]["root"])
+    print(compile_to_visual(elements[item]["root"])[1:-1])
+    print(analyze_root(elements[item]["root"]))
     print('+'.join(elements[item]["base"]))
     print("‾‾‾")
+
+
+def check_combinable(a, b):
+    if type(a) == str and type(b) == str:
+        return len(a) + len(b) <= 4 or (a == b and len(a) >= 3)
+    elif type(a) == str:
+        return len(a) + len(b[0]) <= 4 or (a == b[0] and len(a) >= 3)
+    elif type(b) == str:
+        if type(a[1]) == str:
+            return len(a[1]) + len(b) <= 4 or (a[1] == b and len(b) >= 3)
+        return False
+    else:
+        return len(a[-1]) + len(b[0]) <= 4 or (a[-1] == b[0] and len(a[-1]) >= 3)
+
+
+def combined(a, b):
+    if type(a) == str and type(b) == str:
+        if a == b and len(a) >= 3:
+            return [a, "2"]
+        return a + b
+    elif type(a) == str:
+        if a == b[0] and len(a) >= 3:
+            return [[a, "2"], b[1]]
+        return [a + b[0], b[1]]
+    elif type(b) == str:
+        if a[1] == b and len(b) >= 3:
+            return [a[0], [b, "2"]]
+        return [a[0], a[1] + b]
+    else:
+        if a[1] == b[0] and len(a[1]) >= 3:
+            return [[a[0], [a[1], "2"]], b[1]]
+        return [[a[0], a[1] + b[0]], b[1]]
 
 
 def set_item(item: str, values: list):
@@ -114,14 +222,24 @@ def set_item(item: str, values: list):
     if len(values) == 1 and values[0] in elements.keys():
         elements[item]["root"] = elements[values[0]]["root"]
     elif all(value in elements.keys() for value in values):
-        elements[item]["root"] = ["(" + ''.join(elements[value]["root"]) + ")" for value in values]
+        elements[item]["root"] = [elements[value]["root"] for value in values]
+
+        custom_sort(item)
+        current = elements[item]["root"]
+        if type(current) != str:
+            if len(current) > 1:
+                if check_combinable(current[0], current[1]):
+                    elements[item]["root"] = combined(current[0], current[1])
+            else:
+                elements[item]["root"] = current[0]
+                elements[item]["base"] = [elements[item]["base"][0]]
+
     else:
         for value in values:
             if value not in elements.keys():
                 print(f"{value} not found, please initialize this item before using it to make another.")
 
-    elements[item]["root"] = sorted(elements[item]["root"])
-    elements[item]["base"] = sorted(elements[item]["base"])
+    custom_sort(item)
 
 
 while True:
